bd5289bf6e65bcded655c564b69828fe58784ca5
dozonalx <ukahdozzy@gmail.com>
2022-06-06 00:49:17 -0700

    :frog: task 1. My first toolbox!
    a program that imports functions from the file calculator_1.py, does some Maths, and prints the result

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,13 +1,2 @@
 #!/usr/bin/python3
-def magic_calculation(a, b):
-    from magic_calculation_102 import add, sub
-    if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return c
-    else:
-        return sub(a, b)
-
-# import dis
-# dis.dis(magic_calculation)
+print(str(bytes(list(range(0x41, 0x5B))))[2:28])
