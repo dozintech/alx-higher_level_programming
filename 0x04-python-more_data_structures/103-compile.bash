3715103b2a3a4086f61767e8eb0055c91d720ab3
dozonalx <100067688+dozonalx@users.noreply.github.com>
2022-06-10 06:02:55 +0100

    :bulb: compile bash ...

---
+++ 0x04-python-more_data_structures/103-compile.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+# PY_BIN_PATH=/usr/bin/python3.8
+# PY_INC_PATH=/usr/include/python3.8
+PY_BIN_PATH=/usr/bin/python3.4
+PY_INC_PATH=/usr/include/python3.4
+# PY_BIN_PATH=~/apps/Python-3.4.10/python
+# PY_INC_PATH=~/apps/Python-3.4.10/Include/
+gcc -Wall -Werror -Wextra -pedantic -std=c99 -shared -Wl,-soname,libPython.so -o libPython.so -fPIC -I$PY_INC_PATH 103-python.c
+[ $? -eq 0 ] && $PY_BIN_PATH ./103-tests.py
