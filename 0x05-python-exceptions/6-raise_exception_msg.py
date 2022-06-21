d741a09bb7aedd1f254c4a870288f1879eca9f3f
dozonalx <100067688+dozonalx@users.noreply.github.com>
2022-06-21 05:47:44 +0100

    Create 6-raise_exception_msg.py

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    '''
+    Raises a name exception with a given message
+
+    Parameters:
+    message (str): The exception's message
+
+    Returns:
+    None
+    '''
+    raise NameError(message)
