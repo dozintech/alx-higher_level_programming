50ee76f6f3ff88a5d42c84fc4d0256e21002f758
dozonalx <100067688+dozonalx@users.noreply.github.com>
2022-06-21 05:41:24 +0100

    Create 1-safe_print_integer.py

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    '''
+    Safely prints an integer
+
+    Parameters:
+    value (int): The integer to print
+
+    Returns:
+    True if the integer was successfully printed, otherwise False
+    '''
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception:
+        return False
