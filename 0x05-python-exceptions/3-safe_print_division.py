14d689484a694f6a8f5db2d05522c83061e8750f
dozonalx <100067688+dozonalx@users.noreply.github.com>
2022-06-21 05:45:45 +0100

    Create 3-safe_print_division.py

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    '''
+    Safely prints the quotient of two integers
+
+    Parameters:
+    a (int): The first integer
+    b (int): The second integer
+
+    Returns:
+    The quotient of the two integers, otherwise None
+    '''
+    res = 0
+    try:
+        res = a / b
+    except Exception:
+        res = None
+    finally:
+        print("Inside result: {}".format(res))
+    return res
