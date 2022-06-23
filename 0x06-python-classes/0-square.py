e7c00c693da2dc8d06bdcd34d134821823208d78
dozonalx <ukahdozzy@gmail.com>
2022-06-22 21:52:16 -0700

    we

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+
+This module define empty class Square
+
+"""
+class Square:
+    """
+    Is an empty class of type Square
+    Attributes:
+            None
+    """
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Defines a square by: (based on 0-square.py)
+"""
+
+
+class Square:
+
+    """
+    Define a square class
+    Attributes:
+        None
+    """
+    def __init__(self, size):
+        """
+        Initialization the attributes of Square
+        Args:
+            Size (int): the size of a square
+
+        """
+        self.__size = size
